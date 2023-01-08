74c262e34d3edfc3788eee9f42f0a9597a635971
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2023-01-08 15:09:34 +0000

    sol 3

---
+++ set01/problem03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+# include <stdlib.h>
+
+int main() {
+  int a, b,sum;
+  printf("Enter the first number");
+  if(scanf("%d", &a)!=1)
+    {
+    printf("error");
+    exit(0);
+    }
+  printf("Enter second number");
+  if(scanf("%d", &b)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
+  sum=sum_func(a,b);
+  printf("%d", sum);
+
+  return 0;
+  }
+ int sum_func(int a, int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
