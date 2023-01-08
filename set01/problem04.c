39f96f2f6ef54d477d356e6070ec1b37d431e17f
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2023-01-08 15:10:53 +0000

    sol 4

---
+++ set01/problem04.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int sum_func(int *x,int *y , int *sum);
+
+int main() {
+  int x, y,sum;
+  printf("Enter the first number");
+  if(scanf("%d", &x)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
+  printf("Enter second number");
+  if(scanf("%d", &y)!=1)
+  {
+    printf("error");
+    exit(0);
+  }
+  sum_func(&x, &y, &sum);
+  printf("%d", sum);
+  return 0;
+  }
+
+  sum_func(int *x, int *y, int *sum)
+
+{
+  
+  *sum=*x+*y;
+  
+}
