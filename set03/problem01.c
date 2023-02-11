5ef8f4c2d40cb343cd3ba794305d699a1d54e5c3
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2023-02-11 07:42:22 +0000

    solved

--- set03/problem01.c
+++ set03/problem01.c
@@ -5,8 +5,8 @@ float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
 
 int main(){
-   float x1,x2,y1,y2,distance;
- input(&x1,&y1,&x2,&y2);
+  float x1,x2,y1,y2,distance;
+  input(&x1,&y1,&x2,&y2);
   distance = find_distance(x1,y1,x2,y2);
   output(x1,y1,x2,y2,distance);
 }
@@ -24,7 +24,7 @@ void input(float *x1, float *y1, float *x2, float *y2){
 
 float find_distance(float x1, float y1, float x2, float y2){
   float distance;
-  distance = sqrt( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) );
+  distance = ( ((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)) );
   return distance;
 }
 
