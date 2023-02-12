014390b5565615ec56a984e9c930dfd2e183fb0b
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2023-02-12 13:18:53 +0000

    solved

---
+++ set03/problem07.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<math.h>
+typedef struct point{
+float x, y;
+}Point;
+
+typedef struct line{
+Point p1,p2;
+float distance;
+}Line;
+
+Point input_point();
+Line input_line();
+void find_length(Line *l);
+void output(Line l);
+
+int main(){
+  Line l;
+  l = input_line();
+  find_length(&l);
+  output(l);
+  return 0;
+}
+
+Point input_point(){
+  Point p;
+  printf("enter the point");
+  scanf("%f",&p.x);
+  printf("Enter the point");
+  scanf("%f",&p.y);
+  return p;
+}
+
+Line input_line(){
+  Line l;
+  l.p1 = input_point();
+  l.p2 =  input_point();
+  return l;
+}
+
+void find_length(Line *l){
+  l->distance= (((l->p2.x - l->p1.x))+((l->p2.y - l->p1.x)));
+  printf("distance = %f\n",l->distance);
+}
+
+void output(Line l){
+  printf("The length of line is %.2f\n",l.distance);
+}
