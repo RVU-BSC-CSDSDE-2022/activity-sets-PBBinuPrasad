d1b119efb766f0675a632b9550dd727d6f23474f
P.B Binu Binu <114483084+PBBinuPrasad@users.noreply.github.com>
2023-02-13 06:58:36 +0000

    solved

--- set04/problem02.c
+++ set04/problem02.c
@@ -1,23 +1,25 @@
-#include<stdio.h>
-
+#include <stdio.h>
 typedef struct {
     int num, den;
 } Fraction;
 
-int main(){
-Fraction a = {1,2};
-Fraction b = {1,3};
-Fraction c = {1,4};
-Fraction min = min_fraction(a,b,c);
-printf("The smallest fraction is %dd%",min.num,min.den);
-return 0;
+Fraction min_fraction(Fraction a, Fraction b, Fraction c);
+
+int main() {
+    Fraction a = {1, 2};
+    Fraction b = {1, 3};
+    Fraction c = {1, 4};
+    Fraction min = min_fraction(a, b, c);
+    printf("The smallest fraction is %d/%d\n", min.num, min.den);
+    return 0;
 }
 
-Fraction min_Fraction(Fraction a,Fraction b,Fraction c){
-Fraction min = a;
-if((float) b.num/b.den < (float) min.num/min.den)
-min = b;
-if((float) c.num/c.den < (float) min.num/min.den)
-min = c;
-return min;
+Fraction min_fraction(Fraction a, Fraction b, Fraction c) 
+{
+    Fraction min = a;
+    if ( (float) b.num/b.den < (float) min.num/min.den )
+        min = b;
+    if ( (float) c.num/c.den < (float) min.num/min.den )
+        min = c;
+    return min;
 }
